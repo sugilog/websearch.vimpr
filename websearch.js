6f1ef9f25b9f9bcd897b2bf637f0eb9cf87c16cc
sugilog <sugilog@gmail.com>
2011-04-03 11:36:12 +0900

    Chg: tab open behavior => default true User can change its behavior on template with key => tabOpen { names: ['alc'], description: 'search alc', url: 'http://eow.alc.co.jp/%KEYWORD%/UTF-8/', tabOpen: false} :alc  keyword => current tab :alc! keyword => new tab

--- websearch.js
+++ websearch.js
@@ -10,6 +10,15 @@ var INFO =
   <license>MPL 1.1/GPL 2.0/LGPL 2.1</license>
   <project name="Vimperator" minVersion="2.3"/>
   <item>
+  <tags>:[custom command]</tags>
+  <spec>:[custom command] <a>keyword</a></spec>
+  <description>
+    <p>user custom command to search with keyword on web searvice</p>
+    <p>command without bang will be searched with new tab as default</p>
+    <p>command with bang will be searched with current tab as default</p>
+  </description>
+  </item>
+  <item>
     <tags>webSearchTemplates</tags>
     <spec>webSearchTemplates</spec>
     <description>
@@ -17,10 +26,20 @@ var INFO =
       <code><![CDATA[
 javascript <<EOM
 liberator.globalVariables.webSearchTemplates = [
-  { names: ['alc'], description: 'search alc', url: 'http://eow.alc.co.jp/%KEYWORD%/UTF-8/' }
+  { names: ['alc'], description: 'search alc', url: 'http://eow.alc.co.jp/%KEYWORD%/UTF-8/', tabOpen: true }
 ];
 EOM
       ]]></code>
+      <dl>
+        <dt>names</dt>
+        <dd>command names. (String Array, required)</dd>
+        <dt>description</dt>
+        <dd>command description. (String, optional)</dd>
+        <dt>url</dt>
+        <dd>url for search on web searvice. must have %KEYWORD% to replace keyword. (String, required)</dd>
+        <dt>tabOpen</dt>
+        <dd>tab open setting, default is true. when tabOpen is false, then toggle tab open behavior. (boolean, optional)</dd>
+      </dl>
     </description>
   </item>
 </plugin>
@@ -34,12 +53,12 @@ liberator.plugins.webSearch = (function(){
   }
 
   var templates = [
-    { names: ['alc'], description: 'search alc', url: 'http://eow.alc.co.jp/%KEYWORD%/UTF-8/' }
+    { names: ['alc'], description: 'search alc', url: 'http://eow.alc.co.jp/%KEYWORD%/UTF-8/', tabOpen: true }
   ];
 
   templates = templates.concat(
     liberator.globalVariables.webSearchTemplates.map(function(t){
-      return { names: t.names, description: t.description, url: t.url }
+      return { names: t.names, description: t.description, url: t.url, tabOpen: t.tabOpen }
     })
   );
 
@@ -48,7 +67,7 @@ liberator.plugins.webSearch = (function(){
       template.names,
       template.description,
       function(arg){
-        open(template.url, arg, arg.bang);
+        open(template.url, arg, tabOpenStatus(arg.bang, template.tabOpen));
       },
       {
         argCount : 1,
@@ -63,5 +82,14 @@ liberator.plugins.webSearch = (function(){
       templateUrl.replace(/%KEYWORD%/, keyword),
       ((tabOpen) ? liberator.NEW_TAB : liberator.CURRENT_TAB)
     );
+  }
+
+  var tabOpenStatus = function(bang, userSetting) {
+    if (typeof userSetting == 'undefined') {
+      return !bang;
+    }
+    else {
+      return ((userSetting) ? !bang : bang);
+    }
   }
 })();
